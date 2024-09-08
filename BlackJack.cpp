373004cf391e39fccd7bb284621a5711c574baa9
DragonStar875 <nbpowers2004@gmail.com>
2024-09-08 12:48:15 -0400

    added a bunch

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -45,4 +45,84 @@ class Player{
         void draw(Deck &deck){
             hand.push_back(deck.drawCard());
         }
+
+        void showHand() const {
+            for(const Card& card : hand){
+                cout << card.value << " of " << card.suit << endl;
+            }
+        }
+
+        int findTotal(){
+            int total;
+            for(const Card& card : hand){
+                total += card.value;
+            }
+
+            return total;
+        }
 };
+
+class Game{
+    private:
+        Deck deck;
+        Player player;
+        Player dealer;
+
+        int playerTotal;
+        int dealerTotal;
+    public:
+        void begin(){
+            deck.shuffle();
+
+            player.draw(deck);
+            dealer.draw(deck);
+            player.draw(deck);
+        }
+
+        void hit(Player player){
+            player.draw(deck);
+        }
+
+        void stand(){
+            while(dealer.findTotal() <= 17){
+                hit(dealer);
+            }
+
+            playerTotal = player.findTotal();
+            dealerTotal = dealer.findTotal();
+
+            if(dealerTotal > 21){
+                cout << "Dealer busts! You win!" << endl;
+            }else{
+                if(dealerTotal = 21){
+                    cout << "Dealer has blackjack! You Lose!" << endl;
+                }else{
+                    if(dealerTotal < playerTotal){
+                            cout << "You win!" << endl;
+                    }else{
+                        if(dealerTotal = playerTotal){
+                            cout << "It's a draw!" << endl;
+                        }
+                    }
+                }
+            }
+        }
+};
+
+int main(){
+    Game Game;
+    Game.begin();
+
+    cout << "Your hand:" << endl;
+    player.showHand();
+
+    cout << "Dealer's hand:" << endl;
+    dealer.showHand();
+
+    cout << "What would you like to do?" << endl;
+    cout << "(1) Hit" << endl;
+    cout << "(1) Stand" << endl;
+
+
+    return 0;
+}
