7b29b22362ffdffec42e7459e6f1c28131dd142f
DragonStar875 <nbpowers2004@gmail.com>
2024-09-08 02:33:52 -0400

    modified:   BlackJack.cpp

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -5,6 +5,44 @@
 #include <algorithm>
 using namespace std;
 
-int main(){
-    cout << "Hello World";
-}
+class Card{
+    public:
+        int value;
+        string suit;
+
+        Card(int v, string s) : value(v), suit(s) {}
+};
+
+class Deck{
+    private:
+        vector<Card> cards;
+    public:
+        Deck(){
+            string suits[] = {"Hearts", "Diamonds", "Spades", "Clubs"};
+            for(const string& suit : suits){
+                for(int i = 0; i < 13; ++i){
+                    cards.push_back(Card(i, suit));
+                }
+            }
+        }
+
+        void shuffle(){
+            srand(time(0));
+            random_shuffle(cards.begin(), cards.end());
+        }
+
+        Card drawCard(){
+            Card drawnCard = cards.back();
+            cards.pop_back();
+            return drawnCard;
+        }
+};
+
+class Player{
+    public:
+        vector<Card> hand;
+
+        void draw(Deck &deck){
+            hand.push_back(deck.drawCard());
+        }
+};
